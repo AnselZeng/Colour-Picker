49ed7676002da2bfdcd8ed21dd4b7853ba5b36b1
Ansel Zeng <ansel.zeng.yp@gmail.com>
2020-07-31 22:01:05 -0400

    Changed to palette selection only

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function changeColor() {
     let color = document.getElementById('inputColorPalette').value;
     document.body.style.backgroundColor = color;
+    document.getElementById('colorOutputHEX').value = color;
+}
+
+/*
+function changeColor() {
+    let color = document.getElementById('inputColorText').value;
+    document.body.style.backgroundColor = color;
     document.getElementById('colorOutput').value = color;
 }
+*/
